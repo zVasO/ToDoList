53fd9cec12e5deaaada64941eed7b883b063fc5a
Dylan Germann <dev.dyger@gmail.com>
2022-08-20 12:02:19 +0200

    feat: create TaskFormService and UserFormService, use renderForm instead of render

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,19 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
+use App\Service\FormService\UserFormService;
 use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Form\UserType;
 
 class UserController extends AbstractController
 {
 
 
-    public function __construct(private readonly UserService $userService)
+    public function __construct(private readonly UserService $userService, private readonly UserFormService $userFormService)
     {
     }
 
@@ -23,55 +24,42 @@ class UserController extends AbstractController
     public function showUsersList(): Response
     {
         $users = $this->userService->getAllUsers();
-        return $this->render('user/list.html.twig', ['users' => $users]);
+        return $this->render('user/list.html.twig', [
+            'users' => $users
+        ]);
     }
 
     #[Route('/users/create', name: 'user_create')]
-    public function createUser(Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
+    public function createUser(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
-
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-            $this->userService->createUser($user);
+        if ($this->userFormService->createUser($form, $user)) {
             $this->addFlash('success', "L'utilisateur a bien été ajouté.");
-
             return $this->redirectToRoute('user_list');
         }
 
-        return $this->render('user/create.html.twig', ['form' => $form->createView()]);
+        return $this->renderForm('user/create.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     #[Route('/users/{id}/edit', name: 'user_edit')]
     public function editUser(User $user, Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $form = $this->createForm(UserType::class, $user);
-
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-
-            $this->getDoctrine()->getManager()->flush();
-
+        if ($this->userFormService->editUser($form, $user)) {
             $this->addFlash('success', "L'utilisateur a bien été modifié");
-
             return $this->redirectToRoute('user_list');
         }
 
-        return $this->render('user/edit.html.twig', ['form' => $form->createView(), 'user' => $user]);
+        return $this->renderForm('user/edit.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user
+        ]);
     }
 }
---
+++ src/Service/FormService/UserFormService.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Service\FormService;
+
+use App\Entity\User;
+use App\Service\UserService;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFormService
+{
+
+    public function __construct(private readonly UserService $userService,
+                                private readonly UserPasswordHasherInterface $userPasswordHasher
+    )
+    {
+    }
+
+    /**
+     * @param FormInterface $form
+     * @param User $user
+     * @return bool
+     */
+    public function createUser(FormInterface $form, User $user): bool
+    {
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $this->userPasswordHasher->hashPassword(
+                    $user,
+                    $user->getPassword()
+                )
+            );
+            $this->userService->createUser($user);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param FormInterface $form
+     * @param User $user
+     * @return bool
+     */
+    public function editUser(FormInterface $form, User $user): bool
+    {
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $this->userPasswordHasher->hashPassword(
+                    $user,
+                    $user->getPassword()
+                )
+            );
+            $this->userService->editUser($user);
+            return true;
+        }
+        return false;
+    }
+}
