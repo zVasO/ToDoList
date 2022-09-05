4af49f3a680a9a111faefc750184dafd42e0fbb8
DylanGermann <dev.dyger@gmail.com>
2022-09-05 09:27:38 +0200

    feat: add exception listener + a route who display only the task todo or done

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Exception\AccessException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\Routing\Router;
+use Symfony\Component\Routing\RouterInterface;
+
+/**
+ * @property SessionInterface $session
+ */
+class ExceptionListener
+{
+
+
+    public function __construct(private RouterInterface $router)
+    {
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $session = $event->getRequest()->getSession();
+        // You get the exception object from the received event
+        $exception = $event->getThrowable();
+        dd($exception);
+
+        if ($exception instanceof AccessException) {
+            $session->getFlashBag()->add('error', $exception->getMessage());
+            $event->setResponse(new RedirectResponse($this->router->generate('homepage')));
+        } else {
+            $session->getFlashBag()->add('error', "Une erreur est survenue !");
+            $event->setResponse(new RedirectResponse($this->router->generate('homepage')));
+        }
+    }
+}
